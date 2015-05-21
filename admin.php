40fedacf0a62dd5fd37aed9cf4031eff4c397094
asalimfr <asalimfr@hotmail.com>
2015-05-21 09:33:42 +0300

    Back & Fron Office séparation

--- admin.php
+++ admin.php
@@ -72,7 +72,7 @@ if (defined('ENVIRONMENT'))
  * NO TRAILING SLASH!
  *
  */
-	$application_folder = 'application/frontoffice';
+	$application_folder = 'application/backoffice';
 
 /*
  * --------------------------------------------------------------------
